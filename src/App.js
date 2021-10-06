b8c6027cd80223d01d0439fa0fe580eb3f7901dd
jiko <84399975+jiko97@users.noreply.github.com>
2021-10-06 15:26:27 +0900

    3. Added Props and id, name, image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+// React 장점 : 재사용 가능한 컴포넌트를 만들 수 있음
+// 컴포넌트를 계속 반복해서 사용 가능 
+const foodMyFav = [
+  {
+    id: 1,
+    name: "kimchi",
+    image: "https://images.unsplash.com/photo-1583224944844-5b268c057b72?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8a2ltY2hpfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
+    rating: 5.0
+  },
+  {
+    id: 2,
+    name: "bulgogi",
+    image: "https://images.unsplash.com/photo-1559863658-57587f49d0c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YnVsZ29naXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
+    rating: 4.5
+  },
+  {
+    id: 3,
+    name: "bibimbap",
+    image: "https://images.unsplash.com/photo-1584278858536-52532423b9ea?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YnVsZ29naXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
+    rating: 4.0
+  }
+];
+
+function Food({name, picture, rating}) {
+  return (
+    <div>
+      <h2>I love {name}</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture}></img>
+    </div>    
+  );
+}
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, React!</h1>
+    <div>
+      {foodMyFav.map(dish => (
+        <Food 
+          key={dish.id} 
+          name={dish.name} 
+          picture={dish.image} 
+          rating={dish.rating}>
+        </Food>
+        ))}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
 ReactDOM.render(
+  // component(컴포넌트) -> HTML을 반환하는 함수
+  // JSX -> JS와 HTML 사이의 조합 
   <React.StrictMode>
-    <App />
+  <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
